e84ebad43713ac895a4d706d8f09743c63f3583a
ZohaibKhanDev <mzkhan9610@gmail.com>
2025-05-15 15:23:47 +0500

    python basic

---
+++ 01_intro_to_strings.py
@@ -0,0 +1,12 @@
+name = "Zohaib"
+
+c= 'ABCDEFGHIJKLMNO'
+
+
+print(c[1:5:2])
+
+print(name[0:6])
+
+print(name[:6])
+
+print(name[0:])
---
+++ str_function.py
@@ -0,0 +1,19 @@
+myname = "zohaib"
+
+
+result = myname.endswith("aib")
+
+result1 = myname.startswith("Zoh")
+
+result3 = myname.capitalize()
+
+
+a = "Harry is a Good Boy\nBut Not A Bad \"Boy\""
+
+
+print(result,result1,result3)
+
+print(a)
+
+print("Tab\tSpace")         # Tab
+print("Backslash: \\")      # Backslash
